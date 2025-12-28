9d29297587bc70cce4b8dce8eb53066eb8c94d8b
Oleg Tamanov <olegtamanov@gmail.com>
2025-12-28 23:53:41 +0300

    added logging settings for views.py

--- src/views.py
+++ src/views.py
@@ -1,9 +1,19 @@
 import json
+import logging
 from datetime import datetime
 
 from requests.exceptions import ConnectionError
 
 from src.utils import collect_data_for_main_page
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler("logs/utils.log", "a", encoding="utf-8")
+formatter = logging.Formatter(
+    "%(asctime)s: %(name)s: %(funcName)s: %(levelname)s: %(message)s", datefmt="%Y.%m.%d %H:%M:%S"
+)
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
 
 def get_main_page_data(date: str) -> str:
