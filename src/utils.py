559b8c9f3c43228351ae6f5b6fcb6516c7d65a99
Oleg Tamanov <olegtamanov@gmail.com>
2025-12-28 19:06:18 +0300

    added get_stocks function to get user_stocks

--- src/utils.py
+++ src/utils.py
@@ -47,6 +47,12 @@ def get_currencies(user_settings: dict[str, list[str]]) -> list[str]:
     return result
 
 
+def get_stocks(user_settings: dict[str, list[str]]) -> list[str]:
+    """Получает список акций пользователя для отслеживания курса."""
+    result = user_settings["user_stocks"]
+    return result
+
+
 def load_xlsx_transactions(file_path: str, sheet_name: Union[str, int] = 0) -> pd.DataFrame:
     """Получает данные о транзакциях из xlsx файла и возвращает DataFrame."""
     try:
