cf0bd4e1c3d3e0f7a7ad46d661aa1c15cc26cb01
Oleg Tamanov <olegtamanov@gmail.com>
2025-12-28 21:58:22 +0300

    set ensure_ascii=False for Cyrillic in JSON

--- src/views.py
+++ src/views.py
@@ -11,12 +11,12 @@ def get_main_page_data(date: str) -> str:
     try:
         datetime.strptime(date, "%Y-%m-%d %H:%M:%S")
     except ValueError:
-        return json.dumps({"error": "Неверный формат даты. Ожидается YYYY-MM-DD HH:MM:SS"})
+        return json.dumps({"error": "Неверный формат даты. Ожидается YYYY-MM-DD HH:MM:SS"}, ensure_ascii=False)
 
     try:
         main_page_data = collect_data_for_main_page(date)
     except (ValueError, ConnectionError) as error:
-        return json.dumps({"error": str(error)})
+        return json.dumps({"error": str(error)}, ensure_ascii=False)
 
-    result = json.dumps(main_page_data, indent=4)
+    result = json.dumps(main_page_data, indent=4, ensure_ascii=False)
     return result
