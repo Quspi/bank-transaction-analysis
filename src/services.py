942d29e965b4b8451037b2931685999cdba48c0a
Oleg Tamanov <olegtamanov@gmail.com>
2026-01-02 22:57:34 +0300

    added analyze_cashback_categories to analyze best cashback category

---
+++ src/services.py
@@ -0,0 +1,31 @@
+import json
+
+import pandas as pd
+
+
+def analyze_cashback_categories(data: pd.DataFrame, year: int, month: int) -> str:
+    try:
+        data["Дата операции"] = pd.to_datetime(data["Дата операции"], format="%d.%m.%Y %H:%M:%S")
+        filtered_df = data.loc[
+            (data["Дата операции"].dt.year == year)
+            & (data["Дата операции"].dt.month == month)
+            & (data["Сумма операции"] < 0)
+            & (data["Статус"] == "OK")
+            & (data["Валюта платежа"] == "RUB")
+            & (~data["Категория"].isin(["Наличные", "Переводы"]))
+        ].copy()
+        filtered_df["Сумма операции"] = filtered_df["Сумма операции"].abs()
+
+    except KeyError:
+        raise KeyError("Ошибка в структуре данных.")
+
+    group_by_category = filtered_df.groupby("Категория")["Сумма операции"].sum()
+    top_category = group_by_category.nlargest(3)
+
+    category_dict = {}
+
+    for category, expense in top_category.items():
+        category_dict[category] = round(float(expense) / 100, 2)
+
+    result = json.dumps(category_dict, indent=4, ensure_ascii=False)
+    return result
