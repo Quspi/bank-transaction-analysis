302eebe922425483c2b6e1bc5d3d913ee85bd982
Oleg Tamanov <olegtamanov@gmail.com>
2026-01-02 23:07:37 +0300

    added logging settings

--- src/services.py
+++ src/services.py
@@ -1,6 +1,16 @@
 import json
+import logging
 
 import pandas as pd
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler("logs/utils.log", "a", encoding="utf-8")
+formatter = logging.Formatter(
+    "%(asctime)s: %(name)s: %(funcName)s: %(levelname)s: %(message)s", datefmt="%Y.%m.%d %H:%M:%S"
+)
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
 
 def analyze_cashback_categories(data: pd.DataFrame, year: int, month: int) -> str:
