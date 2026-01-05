b3904d154c78d10c9e864f5c9152b7d5fa5e9945
Oleg Tamanov <olegtamanov@gmail.com>
2026-01-05 18:51:41 +0300

    added save_report decorator to save result to json file

--- src/reports.py
+++ src/reports.py
@@ -1,4 +1,7 @@
 import logging
+from typing import Any, Callable, Optional
+
+import pandas as pd
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
@@ -8,3 +11,22 @@ formatter = logging.Formatter(
 )
 handler.setFormatter(formatter)
 logger.addHandler(handler)
+
+
+def save_report(file_name: Optional[str] = None) -> Callable:
+    """Сохраняет результат работы функций-отчетов в JSON файл."""
+
+    def decorator(function: Callable) -> Callable:
+
+        def wrapper(*args: Any, **kwargs: Any) -> Any:
+            filename = file_name or f"{function.__name__}"
+            result = function(*args, **kwargs)
+
+            if isinstance(result, pd.DataFrame):
+                result.to_json(f"data/{filename}_report.json", orient="records", indent=4, force_ascii=False)
+
+            return result
+
+        return wrapper
+
+    return decorator
