886a53552a8eefc9e928b7d4bf6b999e6a79971f
Oleg Tamanov <olegtamanov@gmail.com>
2026-01-10 17:50:10 +0300

    added tests for load_user_settings

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,7 +1,10 @@
+import json
+from unittest.mock import mock_open, patch
+
 import pytest
 from freezegun import freeze_time
 
-from src.utils import get_greetings
+from src.utils import get_greetings, load_user_settings
 
 
 @pytest.mark.parametrize(
@@ -21,3 +24,38 @@ def test_get_greetings(hour, expected_result):
     with freeze_time(f"2026-01-01 {hour}:00:00"):
         result = get_greetings()
         assert result == expected_result
+
+
+@patch("builtins.open")
+@patch("json.load")
+def test_load_user_settings(mocked_load, mocked_open, valid_settings):
+    mocked_load.return_value = valid_settings
+    result = load_user_settings("test_path")
+    assert result == valid_settings
+    mocked_open.assert_called_once_with("test_path", "r", encoding="utf-8")
+
+
+@patch("builtins.open", side_effect=FileNotFoundError)
+def test_invalid_path_load_user_settings(mocked_open):
+    with pytest.raises(ValueError, match="Файл не найден или удален."):
+        load_user_settings("invalid_path")
+    mocked_open.assert_called_once_with("invalid_path", "r", encoding="utf-8")
+
+
+def test_invalid_json_load_user_settings():
+    with patch("builtins.open", mock_open(read_data="{}")) as mocked_open:
+        with patch("json.load", side_effect=json.JSONDecodeError("error", "doc", 0)) as mocked_load:
+            with pytest.raises(ValueError, match="Файл повреждён или пуст."):
+                load_user_settings("test_path")
+            mocked_open.assert_called_once_with("test_path", "r", encoding="utf-8")
+            mocked_load.assert_called_once()
+
+
+def test_invalid_data_load_user_settings():
+    invalid_data = {"key": []}
+    with patch("builtins.open", mock_open()) as mocked_open:
+        with patch("json.load", return_value=invalid_data) as mocked_load:
+            with pytest.raises(ValueError, match="Ошибка в структуре данных."):
+                load_user_settings("invalid.json")
+                mocked_open.assert_called_once_with("invalid.json", "r", encoding="utf-8")
+                mocked_load.assert_called_once()
