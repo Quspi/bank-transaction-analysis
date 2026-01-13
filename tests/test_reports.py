2fa0235546472281423d799515753983f0feb1c4
Oleg Tamanov <olegtamanov@gmail.com>
2026-01-13 12:48:36 +0300

    added tests for spending_by_category

--- tests/test_reports.py
+++ tests/test_reports.py
@@ -1,8 +1,9 @@
 import os
 
 import pandas as pd
+import pytest
 
-from src.reports import save_report
+from src.reports import save_report, spending_by_category
 
 
 @save_report()
@@ -36,3 +37,28 @@ def test_save_report_no_df():
     result = func_no_df()
     assert not os.path.exists("data/func_no_df_report.json")
     assert isinstance(result, str)
+
+
+def test_spending_by_category(valid_operations):
+    result = spending_by_category(valid_operations, "Супермаркеты", "31-12-2021")
+    result_dict = result.to_dict(orient="records")
+    expected_result = [{"Период": "2021-12", "Сумма": 843.7}]
+    assert result_dict == expected_result
+
+
+def test_invalid_data_spending_by_category(invalid_operations):
+    with pytest.raises(KeyError, match="Ошибка в структуре данных."):
+        spending_by_category(invalid_operations, "Супермаркеты")
+
+
+def test_no_data_spending_by_category(valid_operations):
+    result = spending_by_category(valid_operations, "Несуществующая категория", "31-12-2021")
+    result_dict = result.to_dict(orient="records")
+    expected_result = []
+    assert result.empty
+    assert result_dict == expected_result
+
+
+def test_invalid_date_spending_by_category(valid_operations):
+    with pytest.raises(ValueError, match="Некорректный формат даты"):
+        spending_by_category(valid_operations, "Супермаркеты", "32-21-2021")
