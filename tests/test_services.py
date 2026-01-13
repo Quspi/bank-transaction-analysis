b7d8bd6ce35a01a0df4f26f4b117300f79beb364
Oleg Tamanov <olegtamanov@gmail.com>
2026-01-13 11:05:43 +0300

    added tests for investment_bank

--- tests/test_services.py
+++ tests/test_services.py
@@ -2,7 +2,7 @@ import json
 
 import pytest
 
-from src.services import analyze_cashback_categories
+from src.services import analyze_cashback_categories, investment_bank
 
 
 def test_analyze_cashback_categories(valid_operations):
@@ -36,3 +36,45 @@ def test_empty_result_analyze_cashback_categories(valid_operations):
     result = analyze_cashback_categories(valid_operations, 2025, 1)
     expected_result = json.dumps({}, indent=4, ensure_ascii=False)
     assert result == expected_result
+
+
+def test_investment_bank(dict_transactions):
+    result = investment_bank("2021-12", dict_transactions, 50)
+    expected_result = 672.2
+    assert result == expected_result
+    assert isinstance(result, float)
+
+
+@pytest.mark.parametrize("limit", [-100, -999, -99.2])
+def test_invalid_limit_investment_bank(dict_transactions, limit):
+    with pytest.raises(ValueError, match="limit должен быть положительным числом."):
+        investment_bank("2021-12", dict_transactions, limit)
+
+
+def test_invalid_transactions_investment_bank(invalid_operations):
+    result = investment_bank("2021-12", invalid_operations, 50)
+    expected_result = 0.0
+    assert result == expected_result
+    assert isinstance(result, float)
+
+
+@pytest.mark.parametrize("month", ["2018-12", "2019-12", "2016-12"])
+def test_wrong_date_investment_bank(dict_transactions, month):
+    result = investment_bank(month, dict_transactions, 50)
+    expected_result = 0.0
+    assert result == expected_result
+    assert isinstance(result, float)
+
+
+def test_no_expenses_investment_bank(no_expenses):
+    result = investment_bank("2021-12", no_expenses, 50)
+    expected_result = 0.0
+    assert result == expected_result
+    assert isinstance(result, float)
+
+
+def test_transaction_multiple_investment_bank(transaction_multiple):
+    result = investment_bank("2021-12", transaction_multiple, 50)
+    expected_result = 633.09
+    assert result == expected_result
+    assert isinstance(result, float)
